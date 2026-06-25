e4e32740b12e316213e3b6abd6ebf8c6a6cb5f0e
Karthika RP <karthukarthik242@gmail.com>
2026-06-26 04:37:02 +1030

    Add files via upload

---
+++ overriding.py
@@ -0,0 +1,10 @@
+class Animal:
+    def make_sound(self):
+        print("Animal is making sound")
+    
+class Sound:
+    def make_sound(self):
+        print("Bark")
+        
+d = Sound()
+d.make_sound()
